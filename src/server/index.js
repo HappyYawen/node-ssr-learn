e5e13a7a33b7018dc2dd3baf0f6d9f145b9caee8
karla.guo <karla.guo@ucloud.cn>
2021-01-01 15:49:20 +0800

    模拟数据请求+服务端渲染store数据

--- src/server/index.js
+++ src/server/index.js
@@ -5,29 +5,11 @@
 
 import express from 'express'
 import {render} from './utils'
-// var test = require("../mock/");  //引入具体test的配置
 
 
 const app = express()
 app.use(express.static('public')) //如果路由访问得失静态文件，就去public的目录下去找
 
-// /*为app添加中间件处理跨域请求*/
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-// });
-
-// // 为每个具体的配置创建监听 
-// for(let i=0;i<test.length;i++){
-//     let item = test[i];
-//     app.all(item.router, function(req, res) {
-//         console.log("🚀 ~ file: index.js ~ line 27 ~ app.all ~ item.router", item.router)
-//         res.json(item.data);
-//     });
-// }
 //虚拟Dom是真是Dom的一个javascript对象映射，对服务端渲染提供了很大的便利
 //浏览器端渲染
 // ReactDom.render(<Home/>, document.getElementById('root')) //render方法把虚拟dom转化为真实dom挂载到页面root节点下
